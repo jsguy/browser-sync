651d2f0d9633d25c6c1fa129f05baccd63fd1ec2
jsguy <mikkel@pointful.com>
2016-02-15 22:28:25 +1100

    Add mouseover/mouseout support

--- go.sh
+++ go.sh
@@ -2,4 +2,4 @@ cd ../browser-sync-client
 gulp build
 cd ../browser-sync
 npm install ../browser-sync-client/
-node bin/browser-sync.js start --no-open --proxy local.pc:22222
+node bin/browser-sync.js start --no-open --proxy localhost
